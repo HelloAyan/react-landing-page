0ac1872155d5407eba13ef204fb6a502a5005d76
HelloAyan <18103373@iubat.edu>
2023-10-30 18:19:15 +0600

    navigation completed

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Header.module.css';
+import { Button, Container, Nav, NavDropdown, Navbar, Form } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container >
+                <Navbar.Brand href="#">Logo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="m-auto my-2 my-lg-0"
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action1">About</Nav.Link>
+                        <Nav.Link href="#action1">Contact</Nav.Link>
+                        <Nav.Link href="#action1">Contact</Nav.Link>
+                        <Nav.Link href="#action2">Contact</Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
